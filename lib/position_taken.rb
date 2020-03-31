e3cee1cd139670d5503477610270aaf2ec06180e
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-03-31 16:43:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+
+def position_taken? (board, index)
+ if board[index] == " "
+   puts "false"
+ elsif board[index] == "X"
+   puts "true"
+ else board[index] == nil
+   puts "false"
+ end
+end
