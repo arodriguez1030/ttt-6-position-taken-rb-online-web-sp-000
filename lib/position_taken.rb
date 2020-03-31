810884e31a211e0374e1a70a9d8b4ba2b9112da6
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-03-31 17:22:46 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken? (board, index)
- if board[index] == "X"
-   puts "true"
-  elsif board[index] == "  "
-   puts "false"
- else board[index] == nil
-   puts "false"
+ if board[index] == "X" || board[index] == "O" 
+   true
+ else 
+   false
  end
 end
